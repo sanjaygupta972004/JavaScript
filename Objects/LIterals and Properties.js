61cb7070a23615a58284f161df619237e419dcb8
Shivam KUmar <shivambharadwaj822@gmail.com>
2023-03-28 21:57:19 +0530

    Objects

---
+++ Objects/LIterals and Properties.js
@@ -0,0 +1,23 @@
+let Country = {      // This is an Object.
+    name : "India",  // name is a key which stores the value India.
+    state : 29,      // state is a key which stores 29.
+    capital : "New Delhi" // capital is a key which New Delhi.
+};
+
+// We Can add and remove Property Values
+
+alert(Country.name) // It wll give the Output India and same for all property  which we want to access.
+delete country.capital // It will delete the property capital.
+
+
+
+
+
+// Using MultiWord Property names.
+
+let demo = {
+    name: "Demo",
+    "likes chicken" : true   // Multiword property name should be quoted.
+}
+
+// Last Property in the list may end with a comma but it is not compulsory.
